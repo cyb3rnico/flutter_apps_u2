eff6ed91d6bb3a9480d364548ce26e051ad152e2
Nicolás <cybermodspace@gmail.com>
2021-11-19 21:49:42 -0300

    Ejercicio API DPA

---
+++ u2_app_dpa/android/app/src/main/kotlin/com/example/u2_app_dpa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.u2_app_dpa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ u2_app_dpa/lib/home_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:u2_app_dpa/provider/dpa_provider.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DpaProvider provider = DpaProvider();
+  String region = '', provincia = '', comuna = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('División Político Administrativa'),
+        leading: Icon(Icons.map, color: Colors.white),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            // REGION
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: FutureBuilder(
+                future: provider.getRegiones(),
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text('Cargando Regiones....');
+                  }
+                  return DropdownButton<String>(
+                    // si la region esta vacia asignale null, si no, asignale el valor de la region
+                    value: region.isEmpty ? null : region,
+                    isExpanded: true,
+                    hint: Text('Región'),
+                    items:
+                        snapshot.data.map<DropdownMenuItem<String>>((region) {
+                      return DropdownMenuItem<String>(
+                        child: Text(region['nombre']),
+                        value: region['codigo'],
+                      );
+                    }).toList(),
+                    onChanged: (nuevaRegion) {
+                      setState(() {
+                        region = nuevaRegion.toString();
+                        provincia = '';
+                        comuna = '';
+                      });
+                    },
+                  );
+                },
+              ),
+            ),
+
+            // PROVINCIA
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: FutureBuilder(
+                future: provider.getProvincias(region),
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text('Cargando Provincias...');
+                  }
+                  return DropdownButton<String>(
+                    // si la provincia esta vacia asignale null, si no, asignale el valor de la provincia
+                    value: provincia.isEmpty ? null : provincia,
+                    isExpanded: true,
+                    hint: Text('Provincia'),
+                    items: snapshot.data
+                        .map<DropdownMenuItem<String>>((provincia) {
+                      return DropdownMenuItem<String>(
+                        child: Text(provincia['nombre']),
+                        value: provincia['codigo'],
+                      );
+                    }).toList(),
+                    onChanged: (nuevaProvincia) {
+                      setState(() {
+                        provincia = nuevaProvincia.toString();
+                        comuna = '';
+                      });
+                    },
+                  );
+                },
+              ),
+            ),
+
+            // COMUNA
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: FutureBuilder(
+                  future: provider.getComunas(region, provincia),
+                  builder: (context, AsyncSnapshot snapshot) {
+                    if (!snapshot.hasData) {
+                      return Text('Cargando Comunas...');
+                    }
+                    return ListView.separated(
+                      separatorBuilder: (_, __) => Divider(),
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        if (!snapshot.hasData) {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                        return ListTile(
+                          leading: Icon(MdiIcons.homeCityOutline),
+                          title: Text(snapshot.data[index]['nombre']),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
